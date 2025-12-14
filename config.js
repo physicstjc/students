667fb29cb78fa286b747be175e860c751c3c58dd
physicstjc <85329791+physicstjc@users.noreply.github.com>
2025-12-14 11:03:39 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CONFIG={
- CLIENT_ID:"54665777024-t9j0vcfa93e588i2v8mqo1v771om2nk5.apps.googleusercontent.com",
+ CLIENT_ID:"54665777024-3ei0s9e028rrp2n3oubg7us08ej3t9pn.apps.googleusercontent.com",
  SHEET_ID:"143leHRrSJTzQZFdk8nsfxKMw6t9k30cL7-ecl5ZiyyQ",
  RANGE:"IP3!A:Z"
 };
