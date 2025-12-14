cb35e8374cdd4350adbc8e90a941c75a43d9e899
physicstjc <85329791+physicstjc@users.noreply.github.com>
2025-12-14 13:19:43 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.CONFIG={
- CLIENT_ID:"54665777024-3ei0s9e028rrp2n3oubg7us08ej3t9pn.apps.googleusercontent.com",
- SHEET_ID:"143leHRrSJTzQZFdk8nsfxKMw6t9k30cL7-ecl5ZiyyQ",
- RANGE:"IP3!A:Z"
-};
